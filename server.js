cc43229c0fd2bf293c125b865bba31e4074b6223
josephm <joseph.meszaros89@yahoo.co.uk>
2019-02-17 20:27:50 +0000

    added server

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors')
+var app = express();
+var path = require('path')
+var compression = require('compression')
+var corsOptions = {
+    origin: 'http://localhost:2000',
+    credentials: true,
+
+}
+app.use(cors(corsOptions));
+
+app.use(compression())
+
+app.use(express.static(path.join(__dirname,'client', 'build')));
+
+app.get(function (req, res) {
+  res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+});
+
+
+
+
+var server = app.listen(2000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
